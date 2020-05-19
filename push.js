21c428c4e8484ff94228ac2388cef43193349fa1
jperazas208 <jperaza@mayoreoexp.com>
2020-05-19 11:56:54 -0600

    Cambios en el json

--- push.js
+++ push.js
@@ -2,7 +2,7 @@ const vapid = require("./vapid.json");
 const urlsafeBase64 = require("urlsafe-base64");
 const fs = require("fs");
 
-const suscripciones = [];
+const suscripciones = require("./subs-db.json");
 
 const getKey = () => {
   console.log(vapid.publicKey);
@@ -11,6 +11,8 @@ const getKey = () => {
 
 const addSubscriptcion = (suscripcion) => {
   suscripciones.push(suscripcion);
+
+  console.log(suscripcion);
 
   fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones));
 };
